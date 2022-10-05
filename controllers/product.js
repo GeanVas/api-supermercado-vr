7a2c14b4b340adc423b48e501ed3ac39f1fef645
GeanVas <gean.vasconez@gmail.com>
2022-10-05 17:08:44 -0500

    folder structure

---
+++ controllers/product.js
@@ -0,0 +1,9 @@
+const Product = require('../models/product');
+
+const getProducts = (req, res) => {
+
+}
+
+const getProductById = (req, res) => {
+    
+}
---
+++ models/product.js
@@ -0,0 +1,20 @@
+
+class Product {
+    constructor() {
+
+    }
+
+    getAllProducts(result) {}
+
+    getProductById(id, result) {}
+
+    createProduct(product, result) {}
+
+    editProductById(id, product, result) {}
+
+    deleteProductById(id, result) {}
+
+    deleteProducts(result) {}
+}
+
+module.exports = Product;
