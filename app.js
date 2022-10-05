79123d583d04daf8b92f692bd8520f77eb5e6993
GeanVas <gean.vasconez@gmail.com>
2022-10-05 17:36:04 -0500

    home endpoint

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 8000;
 app.use(cors);
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.send('Manco')
+})
 
 app.listen(PORT, () => console.log("Server on port:", PORT));
