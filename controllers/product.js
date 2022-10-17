b7d592e3658ebec9b8b71d450c932a5acbdd77f6
GeanVas <gean.vasconez@gmail.com>
2022-10-16 19:41:36 -0500

    changed export

--- controllers/product.js
+++ controllers/product.js
@@ -1,18 +1,17 @@
 const sequelize = require('../config/database');
 const Product = sequelize.models.Product;
 
-const getProducts = async (req, res) => {
+exports.getProducts = async (req, res) => {
     const products = await Product.findAll();
     req.json(products);
 }
 
-const getProductById = async (req, res) => {
+exports.getProductById = async (req, res) => {
     const pk = req.params.id;
     const product = await Product.findByPk(pk);
     req.json(product);
 }
 
-module.exports = {
-    getProducts,
-    getProductById
-};
+exports.createProduct = async (req, res) => {
+    // TODO
+}
