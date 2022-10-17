29561c406f64009114ae9db1c38f021eb3ed5611
GeanVas <gean.vasconez@gmail.com>
2022-10-16 19:42:32 -0500

    general routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const product = require('../controllers/product');
+const {isAuth, isAdmin} = require('../middlewares/auth');
+
+router.get('/productos', product.getProducts);
+
+router.get('/producto:id', product.getProductById);
+
+router.post('/producto', product.createProduct);
+
+router.post('/checkout', isAuth, );
