3e591837dba86ed78b8e41a0a680eeb9ff253d6a
GeanVas <gean.vasconez@gmail.com>
2022-10-17 13:37:12 -0500

    test routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const product = require('../controllers/product');
 const {isAuth, isAdmin} = require('../middlewares/auth');
+const sequelize = require('../config/database');
+const Product = sequelize.models.Product;
 
 router.get('/', (req, res) => {
     res.send('Hola Mundo');
 })
 
-router.get('/productos', product.getProducts);
+router.get('/productos', async (req, res) => {
+    const products = await Product.findAll();
+    if (!products) return res.status(404).json({msj: 'No se encuentran productos'});
+    res.json(products);
+});
 
 router.get('/producto:id', product.getProductById);
 
