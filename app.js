bbf18ec32d8e94dccf7472ec5e226f8322058301
GeanVas <gean.vasconez@gmail.com>
2022-11-05 20:34:17 -0500

    cors conf

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const app  = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logger('dev'));
